3325cc90bd6ba7764ccce7c57762ad7c04a3314e
Jeff Capuano <jcapuano328@gmail.com>
2015-04-18 18:25:13 -0400

    accordion type menu navigation: better

--- www/js/mainCtrl.js
+++ www/js/mainCtrl.js
@@ -1,6 +1,6 @@
 angular.module('cwb.controllers')
 
-.controller('MainCtrl', function($rootScope, $scope, $log, Battles) {
+.controller('MainCtrl', function($rootScope, $scope, $location, $log, Battles) {
     $log.info('Load App Controller');
 
 	$scope.save = function() {
@@ -10,11 +10,35 @@ angular.module('cwb.controllers')
 	$scope.reset = function() {
     }
 
+	$scope.select = function(scenario) {
+        //$location.path('/app/scenario/' + scenario.id + '/turn');
+    	$rootScope.$emit('load', scenario.id);
+    }
+    
+    $scope.toggleBattles = function() {
+		$scope.shownBattles = !$scope.shownBattles;
+	}
+    $scope.isBattlesShown = function() {
+    	return !!$scope.shownBattles;
+	}
+    
+    $scope.toggleBattle = function(battle) {
+    	if ($scope.isBattleShown(battle)) {
+        	$scope.shownBattle = null;
+		} else {
+			$scope.shownBattle = battle;
+		}
+	}
+    $scope.isBattleShown = function(battle) {
+    	return $scope.shownBattle === battle;
+	}
+    
     Battles.load()
     .then(function(data) {
         $scope.battles = data;
     })
     .catch(function(err) {
+    	$log.error(err);
     });
 
     // retrieve current game; go to scenario state, etc
--- www/js/scenarioCtrl.js
+++ www/js/scenarioCtrl.js
@@ -3,22 +3,34 @@ angular.module('cwb.controllers')
 .controller('ScenarioCtrl', function($rootScope, $scope, $log, $stateParams, Battles, Current) {
 	$log.info('load scenario controller');
     
+    $rootScope.$on('load', function(e, id) {
+    	load(id);
+    });
+    
     $rootScope.$on('save', function() {
     	$log.info('Save scenario');
         Current.save($scope.current);
     });
     
-    Battles.getScenario($stateParams.scenarioId)
-	.then(function(data) {
-		$log.info('retrieved scenario');
-        $scope.scenario = data;
-        // current scenario settings: turn, orders, roster, etc.
-        $scope.current = Current.load() || Current.new(data)
-	})
-	.catch(function(err) {
-		$log.error('failed to retrieve scenario');
-		$log.error(err);
-        $scope.scenario = {};
-        $scope.current = {};
-	});
+    function load(id) {
+	    Battles.getScenario(id)
+		.then(function(data) {
+			$log.info('retrieved scenario');
+	        $scope.scenario = data;
+	        // current scenario settings: turn, orders, roster, etc.
+	        var current = Current.load();
+	        if (current.scenario != data.id) {
+	        	current = null;
+	        }
+	        $scope.current = current || Current.new(data)
+		})
+		.catch(function(err) {
+			$log.error('failed to retrieve scenario');
+			$log.error(err);
+	        $scope.scenario = {};
+	        $scope.current = {};
+		});
+	}        
+    
+    load($stateParams.scenarioId);
 });
